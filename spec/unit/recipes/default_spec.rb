74baa02d051b250a188c350eea1b567d60164342
Salim Afiune <afiune@chef.io>
2015-10-06 18:22:51 -0500

    Add chefspecs for push-jobs and platform support

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,14 +7,29 @@
 require 'spec_helper'
 
 describe 'delivery-base::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When we run on a supported platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::SoloRunner.new(platform: 'centos', version: '6.5')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'depends on push-jobs' do
+      expect(chef_run).to include_recipe 'push-jobs'
+    end
+  end
+
+  context 'When we run on a NOT supported platform' do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new(platform: 'darwin', version: '10.10')
+      runner.converge(described_recipe)
+    end
+
+    it 'raise an error trying to converge' do
+      expect { chef_run }.to raise_error
     end
   end
 end
